7e1b885ca541c21d938b24d96744f296ca1ddc1b
weelion <weelion@qq.com>
2014-07-09 16:17:14 +0800

    add chinese lang

---
+++ src/lang/cn/all.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+    'remember' => 'Remember me',
+    'signin' => 'Sign in',
+    'email' => 'Email',
+    'password' => 'Password',
+    'error' => 'Error !',
+    'search' => 'Search',
+    'confirm-delete-title' => 'Confirm delete',
+    'confirm-delete-message' => 'Are you sure you want to delete these items ?',
+    'delete' => 'Delete',
+    'new' => 'New',
+    'name' => 'Name',
+    'show' => 'Show',
+    'create' => 'Create',
+    'update' => 'Update',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'messages' => array(
+        'banned' => '用户已冻结',
+        'login-failed' => '账号或密码不正确',
+        'denied' => '抱歉， 您没有权限',
+    ),
+    'titles' => array(
+        'index' => '控制台',
+        'login' => 'Login',
+        'error' => 'Error',
+    ),
+);
---
+++ src/lang/cn/navigation.php
@@ -0,0 +1,9 @@
+<?php 
+
+return array(
+    'index' => '控制台',
+    'users' => '角色',
+    'groups' => '角色组',
+    'permissions' => '权限',
+    'logout' => '登出'
+);
